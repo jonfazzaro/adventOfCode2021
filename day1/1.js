d80acd0320866ebba3a65dd60ced550bb2e70c27
Jon Fazzaro <jon@fazzaro.com>
2021-12-01 09:56:55 -0500

    clean: day 1

--- day1/1.js
+++ day1/1.js
@@ -1,11 +1,7 @@
 module.exports = function increaseCount(sequence){
-    if (sequence.length <= 1)
-        return 0;
-    
     let increases = 0;
     for (let i=1; i<sequence.length; i++) {
-        const diff = sequence[i] - sequence[i-1];
-        if (diff > 0)
+        if (sequence[i] > sequence[i-1])
             increases++;
     }
 
