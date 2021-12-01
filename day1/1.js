305aece94832c31e9bbaa6dba26dfea7d1a060b0
Jon Fazzaro <jon@fazzaro.com>
2021-12-01 13:24:18 -0500

    clean: day 1 FP

--- day1/1.js
+++ day1/1.js
@@ -1,9 +1,5 @@
 module.exports = function increaseCount(sequence){
-    let increases = 0;
-    for (let i=1; i<sequence.length; i++) {
-        if (sequence[i] > sequence[i-1])
-            increases++;
-    }
-
-    return increases;
+    return sequence
+        .filter((n, i) => sequence[i-1] < n)
+        .length;
 }
