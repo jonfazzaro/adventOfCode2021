882e2465db27bb0b709e3220d61c66a8112a8e7c
Jon Fazzaro <jon@fazzaro.com>
2021-12-02 13:29:48 -0500

    day 2, part 1

---
+++ day2/navigate.js
@@ -0,0 +1,45 @@
+module.exports = function navigate(
+  origin = { position: 0, depth: 0 },
+  instructions
+) {
+
+  if (!instructions) return origin;
+
+  const destination = Object.assign({}, origin);
+  instructions.split('\n')
+    .map(i => i.trim())
+    .filter(valid)
+    .forEach(apply);
+  return destination;
+
+  function apply(instruction) {
+    destination[dimension(instruction)] += distance(instruction);
+    normalize(destination);
+  }
+};
+
+function normalize(location) {
+    location.position = Math.max(0, location.position);
+    location.depth = Math.max(0, location.depth);
+}
+
+function dimension(instruction) {
+  return verb(instruction) === "forward" ? "position" : "depth";
+}
+
+function direction(instruction) {
+  return verb(instruction) == "up" ? -1 : 1;
+}
+
+function verb(instruction) {
+  return instruction.split(" ")[0];
+}
+
+function distance(instruction) {
+  return direction(instruction) 
+       * parseInt(instruction.split(" ")[1]);
+}
+
+function valid(i) {
+  return i.split(' ').length == 2;
+}
