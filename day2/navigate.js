4460a7373de0155878683986607c970aba25c792
Jon Fazzaro <jon@fazzaro.com>
2021-12-02 12:55:59 -0500

    wip

--- day2/navigate.js
+++ day2/navigate.js
@@ -12,9 +12,14 @@ module.exports = function navigate(
   function apply(instruction) {
     const dim = dimension(instruction);
     destination[dim] += distance(instruction);
-    destination[dim] = Math.max(0, destination[dim]);
+    normalize(destination);
   }
 };
+
+function normalize(location) {
+    location.position = Math.max(0, location.position);
+    location.depth = Math.max(0, location.depth);
+}
 
 function dimension(instruction) {
   return verb(instruction) === "forward" ? "position" : "depth";
