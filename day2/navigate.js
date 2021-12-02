0d3057d9f5f87aa8513a641a29c5f56517cefa62
Jon Fazzaro <jon@fazzaro.com>
2021-12-02 17:11:53 -0500

    day: 2, pt. 2

--- day2/navigate.js
+++ day2/navigate.js
@@ -1,26 +1,47 @@
-module.exports = function navigate(
-  origin = { position: 0, depth: 0 },
-  instructions
-) {
+module.exports = { navigate, navigateWithAim };
 
+function navigate(
+  origin = { position: 0, depth: 0 },
+  instructions,
+  strategy = standard
+) {
   if (!instructions) return origin;
 
   const destination = Object.assign({}, origin);
-  instructions.split('\n')
-    .map(i => i.trim())
+  instructions
+    .split("\n")
+    .map((i) => i.trim())
     .filter(valid)
-    .forEach(apply);
+    .forEach(strategy(destination));
   return destination;
+}
 
-  function apply(instruction) {
+function navigateWithAim(origin, instructions) {
+  return navigate(origin, instructions, withAim);
+}
+
+function standard(destination) {
+  return (instruction) => {
     destination[dimension(instruction)] += distance(instruction);
     normalize(destination);
-  }
-};
+  };
+}
+
+function withAim(destination) {
+  return (instruction) => {
+    if (verb(instruction) == "forward") {
+      destination.position += distance(instruction);
+      destination.aim = destination.aim || 0;
+      destination.depth += destination.aim * distance(instruction);
+    } else destination.aim += distance(instruction);
+
+    normalize(destination);
+  };
+}
 
 function normalize(location) {
-    location.position = Math.max(0, location.position);
-    location.depth = Math.max(0, location.depth);
+  location.position = Math.max(0, location.position);
+  location.depth = Math.max(0, location.depth);
 }
 
 function dimension(instruction) {
@@ -36,10 +57,9 @@ function verb(instruction) {
 }
 
 function distance(instruction) {
-  return direction(instruction) 
-       * parseInt(instruction.split(" ")[1]);
+  return direction(instruction) * parseInt(instruction.split(" ")[1]);
 }
 
 function valid(i) {
-  return i.split(' ').length == 2;
+  return i.split(" ").length == 2;
 }
