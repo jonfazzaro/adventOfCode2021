810fbc82f508992ffdd5952acdd4ccb30bd3faa5
Jon Fazzaro <jon@fazzaro.com>
2021-12-03 09:04:55 -0500

    wip

--- day3/power.test.js
+++ day3/power.test.js
@@ -29,5 +29,23 @@ describe('The power consumption reader', () => {
        expect(power(readings)).toEqual('01110');
     });
 
+    it('passes the example', () => {
+        const readings = `00100
+        11110
+        10110
+        10111
+        10101
+        01111
+        00111
+        11100
+        10000
+        11001
+        00010
+        01010`
+
+
+       expect(power(readings)).toEqual('10110');
+    });
+
 
 });
