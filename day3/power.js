3a2289acf84e753a963164f069607f0b33b5324b
Jon Fazzaro <jon@fazzaro.com>
2021-12-03 09:11:07 -0500

    wip

--- day3/power.js
+++ day3/power.js
@@ -6,11 +6,11 @@ function toDecimal(value) {
     return parseInt(value.trim() || '0', 2);
 }
 
-function middle(value) {
-    return value[Math.floor(value.length/2)];
-}
-
 function mode(values) {
     values.sort();
     return middle(values); 
+}
+
+function middle(value) {
+    return value[Math.floor(value.length/2)];
 }
