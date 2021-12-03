4df7d1776e1395009f8a98fd982b43e232fddb9b
Jon Fazzaro <jon@fazzaro.com>
2021-12-03 08:44:28 -0500

    wip

--- day3/power.test.js
+++ day3/power.test.js
@@ -1,10 +1,6 @@
 const power = require('./power');
 
 describe('The power consumption reader', () => {
-    it('exists', () => {
-       power() 
-    });
-    
     it('given nothing, returns zero', () => {
         expect(power()).toEqual(0);
     });
