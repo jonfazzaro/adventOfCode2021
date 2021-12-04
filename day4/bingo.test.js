eda47f1ccf9305ae8a2c604314d4d304b4d950cc
Jon Fazzaro <jon@fazzaro.com>
2021-12-04 15:37:19 -0500

    wip

--- day4/bingo.test.js
+++ day4/bingo.test.js
@@ -15,4 +15,15 @@ describe("The bingo game", () => {
       21 22 23 24 25
       `)).toBeNull();
   });
+
+  it('given one board and winning drawings, returns null', () => {
+    expect(bingo(`1, 2, 3, 4, 5
+    
+     1  2  3  4  5 
+     6  7  8  9 10 
+    11 12 13 14 15
+    16 17 18 19 20
+    21 22 23 24 25
+    `)).toBeNull();
+});
 });
