bf2f1bb7de0118ab0257bea1e2c150f4bd3d7e5d
Jon Fazzaro <jon@fazzaro.com>
2021-12-04 16:24:14 -0500

    wip

--- day4/bingo.js
+++ day4/bingo.js
@@ -1,18 +1,38 @@
 module.exports = function bingo(input) {
-    const numbers = drawings(input);
-    if (!!numbers.length)
-        return { 
-            board: 1
-        }
-    return null;
+  if (!input) return null;
+  const drawings = parseDrawings(input);
+  const winners = parseBoards(input).filter(hasBingo(drawings));
+  return winners;
+};
+
+function parseBoards(input) {
+  if (!input) return [];
+  return input.split("\n\n").map(parseBoard);
 }
 
-function drawings(input) {
-    if (!input)
-        return [];
-    return input 
-        .split('\n')[0].trim()
-        .split(',')
-        .map(s => s.trim())
-        .filter(s => !!s); 
+function parseBoard(input) {
+  const boards = elements(input, "\n")[1];
+  return {
+    rows: boards
+      .trim()
+      .split("\n")
+      .map((row) => row.split(" ").filter((s) => !!s)),
+  };
+}
+
+function hasBingo(drawings) {
+  return (board) => true;
+}
+
+function parseDrawings(input) {
+  return elements(elements(input, "\n"), ",");
+}
+
+function elements(input, by) {
+  if (!input) return [];
+  return input
+    .trim()
+    .split(by)
+    .map((s) => s.trim())
+    .filter((s) => !!s);
 }
--- day4/bingo.test.js
+++ day4/bingo.test.js
@@ -1,23 +1,26 @@
 const bingo = require("./bingo");
 
 describe("The bingo game", () => {
-  it("given no boards, returns null", () => {
+  it.only("given no boards, returns null", () => {
     expect(bingo()).toBeNull();
   });
 
-  it('given one board and no drawings, returns null', () => {
-      expect(bingo(`
+  it("given one board and no drawings, returns null", () => {
+    expect(
+      bingo(`
       
        1  2  3  4  5 
        6  7  8  9 10 
       11 12 13 14 15
       16 17 18 19 20
       21 22 23 24 25
-      `)).toBeNull();
+      `)
+    ).toBeNull();
   });
 
-  it('given two boards and winning drawings, returns the winning board', () => {
-    expect(bingo(`  1,5,4,3,2 
+  it("given two boards and winning drawings, returns the winning board", () => {
+    expect(
+      bingo(`  1,5,4,3,2 
     
      1  2  8  4  5 
      6  7  8  9 10 
@@ -30,8 +33,36 @@ describe("The bingo game", () => {
    11 12 13 14 15
    16 17 18 19 20
    21 22 23 24 25
-    `)).toEqual({
-        board: 1
+    `)
+    ).toEqual({
+      board: 1,
     });
-});
+  });
+
+  it("given three boards and winning drawings, returns the winning board", () => {
+    expect(
+      bingo(`  1,5,4,3,2 
+    
+     1  2  8  4  5 
+     6  7  8  9 10 
+    11 12 13 14 15
+    16 17 18 19 20
+    21 22 23 24 25
+
+    1  2 12  4  5 
+    6  7  8  9 10 
+   11 12 13 14 15
+   16 17 18 19 20
+   21 22 23 24 25
+
+    1  2  3  4  5 
+    6  7  8  9 10 
+   11 12 13 14 15
+   16 17 18 19 20
+   21 22 23 24 25
+    `)
+    ).toEqual({
+      board: 2,
+    });
+  });
 });
