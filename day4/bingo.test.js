ffb4acf100764ecd786a2dc9d7b961ce1d903655
Jon Fazzaro <jon@fazzaro.com>
2021-12-04 15:52:16 -0500

    wip

--- day4/bingo.test.js
+++ day4/bingo.test.js
@@ -19,13 +19,13 @@ describe("The bingo game", () => {
   it('given two boards and winning drawings, returns the winning board', () => {
     expect(bingo(`  1, 2, 3, 4, 5
     
-     1  2  3  4  5 
+     1  2  8  4  5 
      6  7  8  9 10 
     11 12 13 14 15
     16 17 18 19 20
     21 22 23 24 25
 
-    1  2  8  4  5 
+    1  2  3  4  5 
     6  7  8  9 10 
    11 12 13 14 15
    16 17 18 19 20
