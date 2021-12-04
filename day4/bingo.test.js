920b281befb3249f3230acff173bf83f65a6df06
Jon Fazzaro <jon@fazzaro.com>
2021-12-04 15:36:25 -0500

    wip

--- day4/bingo.test.js
+++ day4/bingo.test.js
@@ -1,11 +1,19 @@
 const bingo = require("./bingo");
 
 describe("The bingo game", () => {
-  it("exists", () => {
-    bingo();
-  });
-
   it("given no boards, returns null", () => {
     expect(bingo()).toBeNull();
+  });
+
+  it('given one board and no drawings, returns null', () => {
+      expect(bingo(`
+      
+       1  2  3  4  5 
+       6  7  8  9 10 
+      11 12 13 14 15
+      16 17 18 19 20
+      21 22 23 24 25
+      `))
+      
   });
 });
