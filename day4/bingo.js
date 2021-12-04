f03211c0c9bef351a32551392b5814e910469ec2
Jon Fazzaro <jon@fazzaro.com>
2021-12-04 15:47:00 -0500

    wip

--- day4/bingo.js
+++ day4/bingo.js
@@ -1,6 +1,5 @@
 module.exports = function bingo(input) {
     const drawings = (input || '').split('\n')[0];
-    console.log('drawings: ' + drawings);
     if (!!drawings)
         return { 
             board: 0
