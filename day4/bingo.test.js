ae2c93902cbc0923145d3771b14425b05f91f935
Jon Fazzaro <jon@fazzaro.com>
2021-12-07 16:21:40 -0500

    wip

--- day4/bingo.test.js
+++ day4/bingo.test.js
@@ -39,7 +39,7 @@ describe("The bingo game", () => {
   });
 
   it("given three boards and a winning row, returns the winning board", () => {
-    const winner = bingo(`  23, 7, 1,5,19,20,  4,3,2 
+    const winners = bingo(`  23, 7, 1,5,19,20,  4,3,2 
     
     1  2  8  4  5 
     6  7  8  9 10 
@@ -59,12 +59,12 @@ describe("The bingo game", () => {
   16 17 18 19 20
   21 22 23 24 25
    `)
-    expect(winner.index).toEqual(2);
-    expect(winner.draw).toEqual("2");
+    expect(winners[0].index).toEqual(2);
+    expect(winners[0].draw).toEqual("2");
   });
 
   it("given three boards and a winning column, returns the winning board", () => {
-    const winner = bingo(`  3,  8, 13, 23,  18
+    const winners = bingo(`  3,  8, 13, 23,  18
     
     1  2  9  4  5 
     6  7  8  9 10 
@@ -84,13 +84,13 @@ describe("The bingo game", () => {
   16 17 18 19 20
   21 22 23 24 25
    `)
-    expect(winner.index).toEqual(2);
-    expect(winner[0].draw).toEqual("18");
-    expect(winner.score).toEqual(4680);
+    expect(winners[0].index).toEqual(2);
+    expect(winners[0].draw).toEqual("18");
+    expect(winners[0].score).toEqual(4680);
   });
 
   it('matches the example case', () => {
-    const winner = bingo(`7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+    const winners = bingo(`7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
 
     22 13 17 11  0
      8  2 23  4 24
@@ -110,8 +110,8 @@ describe("The bingo game", () => {
     22 11 13  6  5
      2  0 12  3  7`);
     
-    expect(winner[0].index).toEqual(2);
-    expect(winner[0].score).toEqual(4512);
+    expect(winners[0].index).toEqual(2);
+    expect(winners[0].score).toEqual(4512);
   });
 
 });
