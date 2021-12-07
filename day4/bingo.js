e159e06cda12e0bc0dec8e9077434f97c39e750c
Jon Fazzaro <jon@fazzaro.com>
2021-12-07 15:07:51 -0500

    wip

--- day4/bingo.js
+++ day4/bingo.js
@@ -18,9 +18,8 @@ function play(drawing, boards) {
   for (let b = 0; b < boards.length; b++) {
     mark(boards[b], drawing);
 
-    if (hasWon(boards[b])) {
+    if (hasWon(boards[b])) 
       return { ...boards[b], draw: drawing };
-    }
   }
 
   return null;
