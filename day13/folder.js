50c9b133bfdbf6d9a192349ff06f647171ab9e03
Jon Fazzaro <jon@fazzaro.com>
2021-12-13 16:21:16 -0500

    wip

--- day13/folder.js
+++ day13/folder.js
@@ -1,4 +1,4 @@
-module.exports = {parse,print, read, fold} 
+module.exports = { parse, print, read, fold };
 
 function parse(input) {
   const lines = input
@@ -19,33 +19,40 @@ function print(coordinates) {
   );
 }
 
-function read(input){
-    return input
-        .map((line, y) => line.split("")
-            .map((c, x) => readCharacter(c, x, y))
-            .filter(outNullValues))
-                .filter(outNullValues)
-                .flat();
+function read(input) {
+  return input
+    .map((line, y) =>
+      line
+        .split("")
+        .map((c, x) => readCharacter(c, x, y))
+        .filter(outNullValues)
+    )
+    .filter(outNullValues)
+    .flat();
 }
 
 function fold(coordinates, along) {
   // const dimension = along.split('=')[0];
-  const after = parseInt(along.split('=')[1]);
-    return ordered(coordinates.map(c => {
-      return [c[0], c[1] - (2*(c[1]-after)+1)]
-    }).concat(coordinates));
+  const after = parseInt(along.split("=")[1]);
+  return ordered(
+    coordinates
+      .map(c => {
+        return [c[0], c[1] - (2 * (c[1] - after) + 1)];
+      })
+      .concat(coordinates)
+  );
 }
 
 function outNullValues(i) {
-    return !!i;
+  return !!i;
 }
 
 function readCharacter(character, x, y) {
-    return character == "#" ? [x,y] : null;
+  return character == "#" ? [x, y] : null;
 }
 
 function printedLine(coordinates, x, y) {
-    return isIn(coordinates, [x, y]) ? "#" : ".";
+  return isIn(coordinates, [x, y]) ? "#" : ".";
 }
 
 function ys(coordinates) {
@@ -64,9 +71,7 @@ function height(coordinates) {
 }
 
 function isIn(coordinates, coordinate) {
-  return coordinates.some(
-    c => c[0] == coordinate[0] && c[1] == coordinate[1]
-  );
+  return coordinates.some(c => c[0] == coordinate[0] && c[1] == coordinate[1]);
 }
 
 function range(size) {
@@ -75,6 +80,6 @@ function range(size) {
 
 function ordered(coordinates) {
   const clone = [...coordinates];
-  clone.sort((a,b) => a[1] - b[1]);
+  clone.sort((a, b) => a[1] - b[1]);
   return clone;
 }
