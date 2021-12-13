1c17e37b76be6a681e888e9a63d326f349b741c5
Jon Fazzaro <jon@fazzaro.com>
2021-12-13 17:52:11 -0500

    wip

--- day13/folder.test.js
+++ day13/folder.test.js
@@ -112,6 +112,7 @@ describe("The grid folding functions", () => {
 
   it.only('calculates the fold', () => {
     expect(folded(10, 7)).toEqual(4)
+    expect(folded(11, 7)).toEqual(3)
     expect(folded(13, 7)).toEqual(1)
     
   });
