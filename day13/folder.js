76216dd6c834f781eb201ed05d0f515443fe41b3
Jon Fazzaro <jon@fazzaro.com>
2021-12-13 13:15:22 -0500

    wip

---
+++ day13/folder.js
@@ -0,0 +1,11 @@
+
+export function parse(input) {
+  const lines = input
+    .split("\n")
+    .map(s => s.trim())
+    .filter(s => !!s);
+  return lines.map(line => [
+    parseInt(line.split(",")[0]),
+    parseInt(line.split(",")[1]),
+  ]);
+}
