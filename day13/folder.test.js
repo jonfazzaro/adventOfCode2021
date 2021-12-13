51e9e7f0cab240e986c75d3bba5faa4aa7d771ef
Jon Fazzaro <jon@fazzaro.com>
2021-12-13 13:14:19 -0500

    wip

--- day13/folder.test.js
+++ day13/folder.test.js
@@ -6,11 +6,72 @@ describe("The grid parser", () => {
   it("given string input, returns coordinates", () => {
     const input = `0,0
                      1,1`;
-    expect(parse(input)).toEqual([[0, 0],[1, 1]]);
+    expect(parse(input)).toEqual([
+      [0, 0],
+      [1, 1],
+    ]);
+  });
+
+  it("given coordinates, paints a grid", () => {
+    const coordinates = [
+      [2, 3],
+      [4, 5],
+    ];
+
+    expect(print(coordinates)).toEqual([
+      ".....",
+      ".....",
+      ".....",
+      "..#..",
+      ".....",
+      "....#",
+    ]);
   });
 });
 
 function parse(input) {
-  const lines = input.split("\n").map(s => s.trim()).filter(s=>!!s);
-  return lines.map(line => ([ parseInt(line.split(",")[0]), parseInt(line.split(",")[1])]));
+  const lines = input
+    .split("\n")
+    .map(s => s.trim())
+    .filter(s => !!s);
+  return lines.map(line => [
+    parseInt(line.split(",")[0]),
+    parseInt(line.split(",")[1]),
+  ]);
+}
+
+function print(coordinates) {
+  console.log([2, 3]);
+  return range(height(coordinates)).map(y =>
+    range(width(coordinates))
+      .map(x => (isIn(coordinates, [x, y]) ? "#" : "."))
+      .join("")
+  );
+}
+
+function ys(coordinates) {
+  return coordinates.map(c => c[1]);
+}
+
+function xs(coordinates) {
+  return coordinates.map(c => c[0]);
+}
+
+function width(coordinates) {
+  return Math.max(...xs(coordinates)) + 1;
+}
+function height(coordinates) {
+  return Math.max(...ys(coordinates)) + 1;
+}
+
+function isIn(coordinates, coordinate) {
+  const result = coordinates.some(
+    c => c[0] == coordinate[0] && c[1] == coordinate[1]
+  );
+  console.log(`${coordinate} in ${coordinates}? ${result}`);
+  return result;
+}
+
+function range(size) {
+  return [...Array(size).keys()];
 }
