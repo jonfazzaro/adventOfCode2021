d867bc9d59997bebddccce90a200d5d06b791707
Jon Fazzaro <jon@fazzaro.com>
2021-12-13 12:32:35 -0500

    wip

--- day13/folder.test.js
+++ day13/folder.test.js
@@ -1,11 +1,19 @@
 describe("The grid parser", () => {
-    describe("given empty", () => {
-      it("returns empty", () => {
-        expect(parse(``)).toEqual([]);
-      });
-    });
+  it("given empty, returns empty", () => {
+    expect(parse(``)).toEqual([]);
+  });
+
+  it("given string input, returns coordinates", () => {
+    const input = `0,0
+                     1,1`;
+    const result = parse(input);
+    console.log(JSON.stringify([[0, 0],[1, 1]]))
+    expect(JSON.stringify(result))
+        .toEqual(JSON.stringify([[0, 0],[1, 1]]));
+  });
 });
 
-function parse() {
-    return [];
+function parse(input) {
+  const lines = input.split("\n").map(s => s.trim()).filter(s=>!!s);
+  return lines.map(line => ([ parseInt(line.split(",")[0]), parseInt(line.split(",")[1])]));
 }
