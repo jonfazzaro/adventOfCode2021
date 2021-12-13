07b66f0586ed05a5a8e5a17414e21280bb11c15a
Jon Fazzaro <jon@fazzaro.com>
2021-12-13 13:19:36 -0500

    wip

--- day13/folder.test.js
+++ day13/folder.test.js
@@ -1,32 +1,35 @@
-const {parse,print} = require('./folder');
+const { parse, print } = require("./folder");
 
-describe("The grid parser", () => {
-  it("given empty, returns empty", () => {
-    expect(parse(``)).toEqual([]);
+describe("The grid folding functions", () => {
+  describe("when parsing", () => {
+    it("given empty, returns empty", () => {
+      expect(parse(``)).toEqual([]);
+    });
+
+    it("given string input, returns coordinates", () => {
+      const input = `0,0
+            1,1`;
+      expect(parse(input)).toEqual([
+        [0, 0],
+        [1, 1],
+      ]);
+    });
   });
+  describe("when printing", () => {
+    it("given coordinates, prints a grid", () => {
+      const coordinates = [
+        [2, 3],
+        [4, 5],
+      ];
 
-  it("given string input, returns coordinates", () => {
-    const input = `0,0
-                     1,1`;
-    expect(parse(input)).toEqual([
-      [0, 0],
-      [1, 1],
-    ]);
-  });
-
-  it("given coordinates, paints a grid", () => {
-    const coordinates = [
-      [2, 3],
-      [4, 5],
-    ];
-
-    expect(print(coordinates)).toEqual([
-      ".....",
-      ".....",
-      ".....",
-      "..#..",
-      ".....",
-      "....#",
-    ]);
+      expect(print(coordinates)).toEqual([
+        ".....",
+        ".....",
+        ".....",
+        "..#..",
+        ".....",
+        "....#",
+      ]);
+    });
   });
 });
