d211e9a47f15f960a56a593061aac06ef6915322
Jon Fazzaro <jon@fazzaro.com>
2021-12-14 11:49:05 -0500

    day: 14, pt. 1

---
+++ day14/polymer.js
@@ -0,0 +1,78 @@
+const { result } = require("lodash");
+
+module.exports = { formula, differential };
+
+function differential(input) {
+  const grouped = breakdown(input);
+  const values = Object.values(grouped);
+
+  return Math.max(...values) - Math.min(...values);
+}
+
+function formula(polymer, key, depth = 1) {
+  const lookup = toLookup(key);
+
+  return range(depth).reduce((f, i) => iterate(f, lookup), polymer);
+}
+
+function iterate(polymer, lookup) {
+    return polymer
+    .split("")
+    .map(toPairs)
+    .filter(outFalsies)
+    .map(inserted(lookup))
+    .join("");
+}
+
+function inserted(lookup) {
+  return (pair, index) => {
+      let result = lookup[pair] + pair[1];
+    if (index === 0)
+      result = splice(pair.split(""), 1, lookup[pair]).join("");
+
+    return result;
+  };
+}
+
+function toLookup(key) {
+  return key
+    .split("\n")
+    .map(line => line.split("->").map(e => e.trim()))
+    .reduce((o, e) => {
+      o[e[0]] = e[1];
+      return o;
+    }, {});
+}
+
+function toPairs(element, index, list) {
+  if (!isLast(index, list)) {
+    const pair = element + list[index + 1];
+    return pair;
+  }
+}
+
+function outFalsies(e) {
+  return !!e;
+}
+
+function splice(array, index, item) {
+  const cloned = [...array];
+  cloned.splice(index, 0, item);
+  return cloned;
+}
+
+function breakdown(input) {
+  return input.split("").reduce((counts, c) => {
+    if (c in counts) counts[c]++;
+    else counts[c] = 1;
+    return counts;
+  }, {});
+}
+
+function isLast(index, array) {
+  return index === array.length - 1;
+}
+
+function range(size) {
+    return [...Array(size).keys()];
+  }
---
+++ day14/polymer.test.js
@@ -0,0 +1,140 @@
+const {formula,differential} = require('./polymer');
+
+describe("The polymer inserter", () => {
+      const key = `CH -> B
+      HH -> N
+      CB -> H
+      NH -> C
+      HB -> C
+      HC -> B
+      HN -> C
+      NN -> C
+      BH -> H
+      NC -> B
+      NB -> B
+      BN -> B
+      BB -> N
+      BC -> B
+      CC -> N
+      CN -> C`;
+
+  it('given two pairs and a key, inserts according to the key', () => {
+    expect(formula("NNCB", key)).toEqual("NCNBCHB");
+  });
+
+  it('given two pairs and a key, inserts according to the key', () => {
+    expect(formula("NCNBCHB", key)).toEqual("NBCCNBBBCBHCB");
+  });
+
+  it('applies formula insertion multiple times', () => {
+      expect(formula("NNCB", key, 4)).toEqual("NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB")
+  });
+
+  it('computes the differential of a polymer', () => {
+      expect(differential("NBBBCNCCNBBNBNBBCHBHHBCHB")).toEqual(11-4);
+  });
+});
+
+const key = `HK -> C
+SP -> H
+VH -> K
+KS -> B
+BC -> S
+PS -> K
+PN -> S
+NC -> F
+CV -> B
+SH -> K
+SK -> H
+KK -> O
+HO -> V
+HP -> C
+HB -> S
+NB -> N
+HC -> K
+SB -> O
+SN -> C
+BP -> H
+FC -> V
+CF -> C
+FB -> F
+VP -> S
+PO -> N
+HN -> N
+BS -> O
+NF -> H
+BH -> O
+NK -> B
+KC -> B
+OS -> S
+BB -> S
+SV -> K
+CH -> B
+OB -> K
+FV -> B
+CP -> V
+FP -> C
+VC -> K
+FS -> S
+SS -> F
+VK -> C
+SF -> B
+VS -> B
+CC -> P
+SC -> S
+HS -> K
+CN -> C
+BN -> N
+BK -> B
+FN -> H
+OK -> S
+FO -> S
+VB -> C
+FH -> S
+KN -> K
+CK -> B
+KV -> P
+NP -> P
+CB -> N
+KB -> C
+FK -> K
+BO -> O
+OV -> B
+OC -> B
+NO -> F
+VF -> V
+VO -> B
+FF -> K
+PP -> O
+VV -> K
+PC -> N
+OF -> S
+PV -> P
+PB -> C
+KO -> V
+BF -> N
+OO -> K
+NV -> P
+PK -> V
+BV -> C
+HH -> K
+PH -> S
+OH -> B
+HF -> S
+NH -> H
+NN -> K
+KF -> H
+ON -> N
+PF -> H
+CS -> H
+CO -> O
+SO -> K
+HV -> N
+NS -> N
+KP -> S
+OP -> N
+KH -> P
+VN -> H`;
+
+console.log(differential(formula("SHPPPVOFPBFCHHBKBNCV", key, 10)));
+
