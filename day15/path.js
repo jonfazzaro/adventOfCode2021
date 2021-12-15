13d047160345fb9b02860ca05a1dd2bce54543f0
Jon Fazzaro <jon@fazzaro.com>
2021-12-15 13:25:00 -0500

    wip

--- day15/path.js
+++ day15/path.js
@@ -1,22 +1,22 @@
 module.exports = function path(input) {
-    const grid = parseGrid(input);
+  const grid = parseGrid(input);
 
-    const flat = grid.flat();
-    const last = flat.length-1;
-    const end = flat[last];
-    const middle = flat.slice(1,last);
+  const flat = grid.flat();
+  const last = flat.length - 1;
+  const end = flat[last];
+  const middle = flat.slice(1, last);
 
-    return [Math.min(...middle), end]
-}
+  return [Math.min(...middle), end];
+};
 
 function parseGrid(input) {
-    return input.split('\n').map(toRow);
+  return input.split("\n").map(toRow);
 }
 
 function toRow(line) {
-return line.trim().split('').map(toInt)
+  return line.trim().split("").map(toInt);
 }
 
 function toInt(i) {
-    return parseInt(i);
+  return parseInt(i);
 }
