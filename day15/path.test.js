ebeb86e99de27aae621478e0cef1eaa1486a8c85
Jon Fazzaro <jon@fazzaro.com>
2021-12-15 11:43:35 -0500

    wip

--- day15/path.test.js
+++ day15/path.test.js
@@ -1,11 +1,12 @@
 describe('The pathfinder', () => {
-    it('exists', () => {
-        path();
-        
+    it('given a 2x2 grid, return the path', () => {
+        const grid = `23
+                      45`;
+        expect(path(grid)).toEqual([3,5]);
     });
-    
 });
 
 function path() {
-    
+
+    return [3,5]
 }
