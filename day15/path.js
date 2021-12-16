bcfcce34dbe99320db3211ab6433ffb208bfa034
Jon Fazzaro <jon@fazzaro.com>
2021-12-16 17:13:59 -0500

    wip

--- day15/path.js
+++ day15/path.js
@@ -1,39 +1,32 @@
 module.exports = function path(input) {
   const grid = parseGrid(input);
 
-  const lastColumn = grid[0].length-1;
-  const lastRow = grid.length-1;
+  const lastColumn = grid[0].length - 1;
+  const lastRow = grid.length - 1;
 
-  let position = {x:0,y:0};
+  let position = { x: 0, y: 0 };
   const values = [];
-  while(!isLast(position)) {
-    
+  while (!isLast(position)) {
     const right = moveRight(position, lastColumn);
-    const down = moveDown(position, lastRow); 
+    const down = moveDown(position, lastRow);
     position = nextMove(right, down);
-    
-       if (!!position)
 
-    values.push(value(grid, position));
+    if (!!position) values.push(value(grid, position));
   }
 
   return values;
 
   function isLast(position) {
-    return !!position 
-        && position.x === lastColumn 
-        && position.y === lastRow;
+    return !!position && position.x === lastColumn && position.y === lastRow;
   }
-  
+
   function nextMove(a, b) {
     let result = null;
 
-    if (a && (isLast(a) || value(grid, a) < value(grid, b)))
-      result = a;
-    
-    if (b && (isLast(b) || value(grid, b) < value(grid, a)) )
-      result = b;
-      
+    if (a && (isLast(a) || value(grid, a) < value(grid, b))) result = a;
+
+    if (b && (isLast(b) || value(grid, b) < value(grid, a))) result = b;
+
     return result;
   }
 };
@@ -43,29 +36,29 @@ function parseGrid(input) {
 }
 
 function toRow(line) {
-  return line.trim().split("").map(i => parseInt(i));
+  return line
+    .trim()
+    .split("")
+    .map(i => parseInt(i));
 }
 
 function value(grid, position) {
-  if (!position)
-    return -1;
+  if (!position) return -1;
   return grid[position.x][position.y];
 }
 
 function moveRight(position, limit) {
-  if (!position || limit<position.x+1)
-    return null;
+  if (!position || limit < position.x + 1) return null;
   return {
-    x: position.x+1,
-    y: position.y
-  }
+    x: position.x + 1,
+    y: position.y,
+  };
 }
 
 function moveDown(position, limit) {
-  if (!position || limit<position.y+1)
-    return null;
+  if (!position || limit < position.y + 1) return null;
   return {
     x: position.x,
-    y: position.y+1
-  }
+    y: position.y + 1,
+  };
 }
